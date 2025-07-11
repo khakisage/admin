e970edfb23d8c3307e85e24c0f52470f2ba27638
khakisage <jongjin201@gmail.com>
2025-07-11 10:38:52 +0900

    firebase 메시징 서비스 설정 추가 및 .gitignore 파일 업데이트

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 
 // API 인스턴스 생성
 export const api = axios.create({
-  //baseURL: "http://localhost:8000/api",
+  // baseURL: "http://localhost:8000/api",
   baseURL: "https://www.wooricenter.co.kr/api",
   headers: {
     "Content-Type": "application/json",
